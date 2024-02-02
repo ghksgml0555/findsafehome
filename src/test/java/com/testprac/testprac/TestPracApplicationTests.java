e1c910d9d54cf7163ae5a8c77b3723eddbef35ea
swnam <swnam@12cm.co.kr>
2024-02-02 12:56:37 +0900

    초기세팅

---
+++ src/test/java/com/testprac/testprac/TestPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testprac.testprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
