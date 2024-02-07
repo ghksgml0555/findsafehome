a643e85a7920b4a8ca28d98d5e1f6d51b844d94d
swnam <swnam@12cm.co.kr>
2024-02-07 14:57:48 +0900

    [프로젝트 이름 수정]

---
+++ src/main/java/com/swyg/findingahomesafely/FindingAHomeSafelyApplication.java
@@ -0,0 +1,13 @@
+package com.swyg.findingahomesafely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingAHomeSafelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindingAHomeSafelyApplication.class, args);
+	}
+
+}
